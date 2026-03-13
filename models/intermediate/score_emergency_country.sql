7feb6892e43dcaf4bfb4a19081609b500519ce13
Adeline DE CIZANCOURT <adelinedecizancourt@gmail.com>
2026-03-13 11:00:03 +0000

    Commit

--- models/intermediate/score_emergency_country.sql
+++ models/intermediate/score_emergency_country.sql
@@ -1,9 +1,7 @@
 SELECT 
-    country
-    -- Calcul de la moyenne pondérée
-    ROUND(SUM(final_score * rs) / NULLIF(SUM(rs), 0), 2) AS global_country_score,
-    -- Nombre total d'événements par pays
+    country,
+    ROUND(SUM(final_score * rs) / NULLIF(SUM(rs), 0),2) AS global_country_score,
     COUNT(*) AS total_events_count
 FROM {{ ref('score_emergency') }}
-GROUP BY 1
+GROUP BY country
 ORDER BY global_country_score DESC

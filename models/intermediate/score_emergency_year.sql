a67a02da5eb935f4c720f2e4893c7a6c0a1d538b
Adeline DE CIZANCOURT <adelinedecizancourt@gmail.com>
2026-03-13 14:44:07 +0000

    J'arrête avec year

--- models/intermediate/score_emergency_year.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    country,
-    "year",
-    ROUND(SUM(final_score * rs) / NULLIF(SUM(rs), 0), 2) AS global_country_score,
-    COUNT(*) AS total_events_count
-FROM {{ ref('score_emergency') }}
-GROUP BY 1,2
-ORDER BY global_country_score DESC

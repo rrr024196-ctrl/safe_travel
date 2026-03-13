1e1688a5dc48409d9e9da842dc3202d6477c667e
NICOLAS MOUCHON <nicolas.mouchon@gmail.com>
2026-03-13 09:32:43 +0000

    Add tables indicators

---
+++ models/staging/safetravel/stg_safetravel__world_development_scored.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('safetravel', 'world_development_scored') }}
+
+),
+
+renamed as (
+
+    select
+        country,
+        year,
+        indice_exposition,
+        indice_economie,
+        indice_resilience,
+        indice_risque_catastrophe,
+        indice_risque_catastrophe_100
+
+    from source
+
+)
+
+select * from renamed

8019c49ee20643effab9b37e056e4613baf811c1
NICOLAS MOUCHON <nicolas.mouchon@gmail.com>
2026-03-14 19:29:54 +0000

    c

---
+++ models/intermediate/int_world_dev_cata.sql
@@ -0,0 +1,13 @@
+
+select
+r.*,
+e.disaster, 
+e.tot_events, 
+e.tot_affected,
+e.tot_deaths, 
+e.damage_usd,
+
+from {{ ref('stg_safetravel__world_development_short') }} as r
+left join {{ ref('stg_safetravel__emergency') }} as e
+    on r.year = e.year
+    and r.country = e.country
